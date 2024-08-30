2fba891ef5fb2ed48e940285fcbb8b7ca74b2635
joaovictor-ferreira <joaovictor2111@gmail.com>
2024-08-30 19:43:27 -0300

    feat: login

--- src/presentation/views/Login/Login.tsx
+++ src/presentation/views/Login/Login.tsx
@@ -1,3 +1,101 @@
+'use client';
+
+import { CssBaseline, Paper, Box, Grid } from '@mui/material';
+import Link from 'next/link';
+import { useState } from 'react';
+import tagLogoOrange from '@/presentation/assets/tag-logo-orange.png';
+import { Button } from '@/presentation/components/Button/Button';
+import { LoginStyles } from '@/presentation/views/Login/styles';
+// import { useLogin } from './hooks/useLogin';
+import { Input } from '@/presentation/components/Input/Input';
+
 export const Login: React.FC = () => {
-  return <div>Login</div>;
+  // const { errors, handleInputChange, handleSubmit } = useLogin();
+
+  return (
+    <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={6}
+        sx={{
+          backgroundImage: 'url(https://picsum.photos/1920/1080)',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+      <CssBaseline />
+      <Grid item xs={12} sm={8} md={6} component={Paper} elevation={6}>
+        <Box
+          sx={{
+            my: 5,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            height: '100%',
+            padding: { md: '12px 48px', xs: 'unset' },
+          }}
+        >
+          <LoginStyles.LogoContainer>
+            <LoginStyles.TagLogo src={tagLogoOrange} alt="Take a Guide Icon" />
+
+            <LoginStyles.Title>Login to your account</LoginStyles.Title>
+          </LoginStyles.LogoContainer>
+
+          <Box
+            component="form"
+            noValidate
+            // onSubmit={handleSubmit}
+            sx={{ mt: 1 }}
+            width="100%"
+          >
+            <>
+              <LoginStyles.InputContainer>
+                <Input
+                  placeholder="Email"
+                  // onChange={handleInputChange}
+                  name="email"
+                  required
+                  // errorMessage={errors.email}
+                />
+                <Input
+                  placeholder="Password"
+                  // onChange={handleInputChange}
+                  type="password"
+                  name="password"
+                  required
+                  // errorMessage={errors.password}
+                />
+              </LoginStyles.InputContainer>
+
+              <Grid item xs={12} marginTop={2}>
+                <Button.Primary $width="100%">Login</Button.Primary>
+              </Grid>
+
+              <Grid
+                container
+                sx={{ justifyContent: 'center', marginTop: '.5rem' }}
+              >
+                <Grid item>
+                  Don't have an account?
+                  <Link
+                    href="/signup"
+                    style={{
+                      textDecoration: 'underline',
+                      marginLeft: '0.25rem',
+                    }}
+                  >
+                    {'Sign Up'}
+                  </Link>
+                </Grid>
+              </Grid>
+            </>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+  );
 };
---
+++ src/presentation/views/Login/styles.tsx
@@ -0,0 +1,64 @@
+import styled from '@emotion/styled';
+import Image from 'next/image';
+
+const Title = styled.h1`
+  font-size: 1.25rem;
+  color: ${({ theme }) => theme.colors.secondary['blue']};
+  text-align: left;
+  margin: 0 0 0 0;
+  line-height: 32px;
+  font-weight: 700;
+
+  @media (max-width: 768px) {
+    font-size: 1.5rem;
+  }
+`;
+
+const TagLogo = styled(Image)`
+  width: 7rem;
+  height: auto;
+  object-fit: contain;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  gap: 2rem;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  width: 100%;
+`;
+
+const Back = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 0 0.25rem;
+  font-size: 1rem;
+  font-weight: 700;
+  margin: -0.25rem 0 0.25rem 0;
+  color: ${({ theme }) => theme.colors.primary['01']};
+  cursor: pointer;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  width: 100%;
+  margin: 1rem 0 2rem 0;
+
+  > input {
+    width: 100%;
+  }
+`;
+
+export const LoginStyles = {
+  Title,
+  LogoContainer,
+  TagLogo,
+  Back,
+  InputContainer,
+};
